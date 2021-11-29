4878cfa61c7d0f9432562060d7f314a29624a103
chx-chx <60890336+chx-chx@users.noreply.github.com>
2021-11-29 18:12:42 +0800

    11.29第二次
    11.29第二次

--- day01/01_helloword.py
+++ day01/01_helloword.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+bp = Blueprint('bp', __name__)
+
+# class HelloWorldResource(Resource):
+#     def get(self):
+#         return {'hello': 'world'}
+#
+#     def post(self):
+#         return {'msg': 'post hello world'}
+
+api = Api(bp)
+
+
 class HelloWorldResource(Resource):
     def get(self):
         return {'hello': 'world'}
 
-        def post(self):
+    def post(self):
         return {'msg': 'post hello world'}
 
+
 api.add_resource(HelloWorldResource, '/')
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+app.register_blueprint(bp)
